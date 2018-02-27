fa6bf8a6bc7069aaa494c52759b67e0405376492
Marcos <vmvwebworks@gmail.com>
2018-02-27 18:47:46 +0100

    social login

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,16 @@ ActiveRecord::Schema.define(version: 20180227151536) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "last_name"
+    t.string "profile_image"
+    t.string "email_verified"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
